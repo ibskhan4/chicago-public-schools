c9ab2878474c1f52e4c2e593bb825654383ceabc
Ibraheem Khan <ibraheemkhan@Ibraheems-MacBook-Pro.local>
2020-04-05 20:05:36 -0500

    Final commit

---
+++ graph/app.R
@@ -0,0 +1,34 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+        plotOutput("image")
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    output$image <- renderImage({
+        # Return a list containing the filename
+        list(src = "graph.png",
+             width = 800,
+             height = 800,
+             contentType = 'image/png',
+             alt = "This is alternate text"
+        )}, deleteFile = FALSE)
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
